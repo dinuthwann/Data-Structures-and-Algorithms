3c145d9f6e01305e6f839389241dd0982f32f52d
dinuthwann <dinuthwann@gmail.com>
2026-09-16 17:20:56 +0530

    Fix bugs and add menu-driven interface for singly linked list

--- linkedlist1.c
+++ linkedlist1.c
@@ -1,55 +1,73 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-/*first we create a nofe with a two parts it is a data part and the pointer part*/
-struct node                   
-{
-    /* data */
-    int data;
-    struct node *next; // this struct use to pointer to the next node
-    
-};
-struct node *head =NULL; // head is note a data part its point to the first node of the linked list that why we use *sign before the head
+// Function Prototypes
+struct node* insert_beg(struct node *head, int val);
+struct node* insert_end(struct node *head, int val);
+void display(struct node *head);
 
-/*inserting the node */
-struct node*instert_beg(struct node*head,int val ){
+/* First we create a node with two parts: data part and the pointer part */
+struct node 
+{
+    int data;
+    struct node *next; // This struct is used to point to the next node
+};
+
+struct node *head = NULL; // head is not a data part, it points to the first node of the linked list
+
+/* Inserting the node at the beginning */
+struct node* insert_beg(struct node *head, int val) {
     struct node *first_node;
-    printf("\n Enter the data:");
-    scanf("%d",&val);
+    printf("\nEnter the data: ");
+    scanf("%d", &val);
+    
     first_node = (struct node*)malloc(sizeof(struct node));
-    first_node -> data =val;
-    first_node -> next =NULL;
-    head = first_node;
+    first_node->data = val;
+    first_node->next = head; // Point the new node to the current head
+    head = first_node;       // Make the new node as the head
+    
     return head;
-    
 }
-struct node*instert_end(struct node *head,int val){
-    struct node *ptr ,*first_node;
-    printf("\n Enter the data : ");
-    scanf("%d",&val);
+
+/* Inserting the node at the end */
+struct node* insert_end(struct node *head, int val) {
+    struct node *ptr, *first_node;
+    printf("\nEnter the data: ");
+    scanf("%d", &val);
+    
     first_node = (struct node*)malloc(sizeof(struct node));
-    first_node ->data= val;
-    first_node ->next=NULL;
-    ptr = head;
-    while (ptr->next!= NULL)
-    {
-        /* code */
-        ptr = ptr ->next;
-        ptr ->next= first_node;
+    first_node->data = val;
+    first_node->next = NULL;
+    
+    // If the linked list is empty
+    if (head == NULL) {
+        head = first_node;
+        return head;
     }
     
-}
-void display(struct node*head){
-    struct node* ptr;
     ptr = head;
-    while (ptr!=NULL)
-    {
-        /* code */
-        printf("%d->",ptr->data);
-        ptr=ptr->next;
+    // Traverse until the last node
+    while (ptr->next != NULL) {
+        ptr = ptr->next;
     }
+    ptr->next = first_node; // Link the last node to the new node
     
+    return head;
 }
+
+/* Display the linked list */
+void display(struct node *head) {
+    struct node *ptr;
+    ptr = head;
+    printf("\nLinked List: ");
+    while (ptr != NULL) {
+        printf("%d -> ", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("NULL\n");
+}
+
+/* Main function to test the program */
 int main() {
     int choice, val = 0;
 
